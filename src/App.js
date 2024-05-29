c153edbbdbfe8768337c651743853ddd872a55b6
Lolitaklim <lolita.klim@yandex.com>
2024-05-29 23:00:34 +0400

    users

--- src/App.js
+++ src/App.js
@@ -1,17 +1,54 @@
-import React from 'react';
-import './index.scss';
-import { Success } from './components/Success';
-import { Users } from './components/Users';
+import { useEffect, useState } from 'react'
+import './index.scss'
+import { Success } from './components/Success'
+import { Users } from './components/Users'
 
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+  const [users, setUsers] = useState([])
+  const [isLoading, setLoading] = useState(true)
+  const [searchValue, setSearchValue] = useState('')
+  const [invites, setInvites] = useState([])
+  const [success, setSuccess] = useState(false)
+
+  useEffect(() => {
+    fetch('https://reqres.in/api/users')
+      .then((res) => res.json())
+      .then((json) => setUsers(json.data))
+      .catch((err) => console.log(err))
+      .finally(() => setLoading(false))
+  }, [])
+
+  const onChangeSearchValue = (e) => setSearchValue(e.target.value)
+
+  const onClickInvite = (id) => {
+    if (invites.includes(id)) {
+      setInvites((prev) => prev.filter((_id) => _id !== id))
+    } else {
+      setInvites((prev) => [...prev, id])
+    }
+  }
+
+  const onClickSuccess = () => setSuccess(true)
+
   return (
     <div className="App">
-      <Users />
-      {/* <Success /> */}
+      {success ? (
+        <Success count={invites.length} />
+      ) : (
+        <Users
+          onChangeSearchValue={onChangeSearchValue}
+          searchValue={searchValue}
+          items={users}
+          isLoading={isLoading}
+          invites={invites}
+          onClickInvite={onClickInvite}
+          onClickSuccess={onClickSuccess}
+        />
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Users/Skeleton.jsx
+++ src/components/Users/Skeleton.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import ContentLoader from 'react-content-loader';
+import React from 'react'
+import ContentLoader from 'react-content-loader'
 
 export const Skeleton = () => (
   <ContentLoader
@@ -8,10 +8,11 @@ export const Skeleton = () => (
     height={50}
     viewBox="0 0 320 50"
     backgroundColor="#f3f3f3"
-    foregroundColor="#ecebeb">
+    foregroundColor="#ecebeb"
+  >
     <circle cx="25" cy="25" r="25" />
     <rect x="64" y="0" rx="6" ry="6" width="137" height="25" />
     <rect x="64" y="32" rx="6" ry="6" width="183" height="15" />
     <rect x="290" y="12" rx="6" ry="6" width="26" height="26" />
   </ContentLoader>
-);
+)

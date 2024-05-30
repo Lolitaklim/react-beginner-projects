04c78138dedf15977cf84509421e73780d0a4914
Lolitaklim <lolita.klim@yandex.com>
2024-05-30 23:46:44 +0400

    photos

--- src/App.js
+++ src/App.js
@@ -1,52 +1,83 @@
-import React from 'react';
-import './index.scss';
+import React, { useEffect, useState } from 'react'
+import './index.scss'
+import Collection from './Collection'
 
-function Collection({ name, images }) {
-  return (
-    <div className="collection">
-      <img className="collection__big" src={images[0]} alt="Item" />
-      <div className="collection__bottom">
-        <img className="collection__mini" src={images[1]} alt="Item" />
-        <img className="collection__mini" src={images[2]} alt="Item" />
-        <img className="collection__mini" src={images[3]} alt="Item" />
-      </div>
-      <h4>{name}</h4>
-    </div>
-  );
-}
+const cats = [
+  { name: 'Все' },
+  { name: 'Море' },
+  { name: 'Горы' },
+  { name: 'Архитектура' },
+  { name: 'Города' },
+]
 
 function App() {
+  const [collections, setCollections] = useState([])
+  const [searchValue, setSearchValue] = useState('')
+  const [categoryId, setCategoryId] = useState(0)
+  const [isLoading, setLoading] = useState(false)
+  const [page, setPage] = useState(1)
+
+  useEffect(() => {
+    setLoading(true)
+    fetch(
+      `https://6658c47c5c3617052649c5ee.mockapi.io/collections?page=${page}&limit=3&${
+        categoryId ? `category=${categoryId}` : ''
+      }`
+    )
+      .then((res) => res.json())
+      .then((json) => setCollections(json))
+      .catch((err) => console.log(err))
+      .finally(() => setLoading(false))
+  }, [categoryId, page])
+
   return (
     <div className="App">
       <h1>Моя коллекция фотографий</h1>
       <div className="top">
         <ul className="tags">
-          <li className="active">Все</li>
-          <li>Горы</li>
-          <li>Море</li>
-          <li>Архитектура</li>
-          <li>Города</li>
+          {cats.map((value, index) => (
+            <li
+              onClick={() => setCategoryId(index)}
+              className={categoryId === index ? 'active' : ''}
+              key={index}
+            >
+              {value.name}
+            </li>
+          ))}
         </ul>
-        <input className="search-input" placeholder="Поиск по названию" />
-      </div>
-      <div className="content">
-        <Collection
-          name="Путешествие по миру"
-          images={[
-            'https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1560840067-ddcaeb7831d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1531219572328-a0171b4448a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1573108724029-4c46571d6490?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-          ]}
+        <input
+          onChange={(e) => setSearchValue(e.target.value)}
+          value={searchValue}
+          className="search-input"
+          placeholder="Поиск по названию"
         />
       </div>
+      <div className="content">
+        {isLoading ? (
+          <h2>Загрузка...</h2>
+        ) : (
+          collections
+            .filter((value) => {
+              return value.name.toLowerCase().includes(searchValue)
+            })
+            .map((value, index) => (
+              <Collection key={index} name={value.name} images={value.photos} />
+            ))
+        )}
+      </div>
       <ul className="pagination">
-        <li>1</li>
-        <li className="active">2</li>
-        <li>3</li>
+        {[...Array(5)].map((_, index) => (
+          <li
+            key={index}
+            onClick={() => setPage(index + 1)}
+            className={page === index + 1 ? 'active' : ''}
+          >
+            {index + 1}
+          </li>
+        ))}
       </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
